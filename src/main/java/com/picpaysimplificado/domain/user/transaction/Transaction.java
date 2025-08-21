7aa4610fc1b515e277fcd843fc65d8b064717243
Adna Backman <adnabackman@gmail.com>
2025-08-21 08:48:53 -0300

    criando o transaction

--- src/main/java/com/picpaysimplificado/domain/user/transaction/Transaction.java
+++ src/main/java/com/picpaysimplificado/domain/user/transaction/Transaction.java
@@ -1,9 +1,40 @@
 package com.picpaysimplificado.domain.user.transaction;
 
-import jakarta.persistence.Entity;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
-@Entity(name="transaction")
+import com.picpaysimplificado.domain.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+
 
 public class Transaction {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal amount;
+	@ManyToOne
+	@JoinColumn(name="sender.id")
+	private User sender;
+	@ManyToOne
+	@JoinColumn(name="receiver.id")
+	private User receiver;
+	private LocalDateTime timestamp;
 }
+
