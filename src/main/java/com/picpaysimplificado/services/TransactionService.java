fbe8cadff0ded4c317b72d6e526bef7164b3d86f
Adna Backman <adnabackman@gmail.com>
2025-08-22 11:10:06 -0300

    creat: transaction service

---
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -0,0 +1,58 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.transaction.Transaction;
+import com.picpaysimplificado.dtos.TransactionDTO;
+import com.picpaysimplificadore.repositories.TransactionRepository;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class TransactionService {
+	@Autowired
+	private UserServices userService;
+	
+	@Autowired
+	private TransactionRepository repository;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	public void createTransaction(TransactionDTO transaction) throws Exception { 		
+		User sender = this.userService.findUserById(transaction.senderId());
+		User receiver = this.userService.findUserById(transaction.receiverId());
+		
+		userService.validateTransaction(sender, transaction.value());
+	
+		boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
+		if(!isAuthorized) {
+			throw new Exception("Transação não autorizada");
+		}
+		
+		Transaction newTransaction = new Transaction();
+		newTransaction.setAmount(transaction.value());
+		newTransaction.setSender(sender);
+		newTransaction.setReceiver(receiver);
+		newTransaction.setTimestamp(LocalDateTime.now());
+	
+	}
+
+	public boolean authorizeTransaction(User sender, BigDecimal value) {
+	    ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/8fafdd68-a090-696f-8c9a-3442cf30dae6", Map.class);
+	    
+	    if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+	    	String message = (String)authorizationResponse.getBody().get("message");
+	        return "Autorizado".equalsIgnoreCase(message);
+	    } else {
+	        return false;
+		}
+	}
+	
+} 
+
