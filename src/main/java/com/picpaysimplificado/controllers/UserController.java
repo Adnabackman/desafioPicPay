09f08d74cab7ffc0d09f821c44252761cd5a1914
Adna Backman <adnabackman@gmail.com>
2025-08-25 08:02:35 -0300

    fix: arrumando código

--- src/main/java/com/picpaysimplificado/controllers/UserController.java
+++ src/main/java/com/picpaysimplificado/controllers/UserController.java
@@ -8,13 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.picpaysimplificado.domain.user.User;
 import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.services.UserService;
 
-@RestController("/users")
+@RestController()
+@RequestMapping("/users")
+
 public class UserController {
 	
 	@Autowired
