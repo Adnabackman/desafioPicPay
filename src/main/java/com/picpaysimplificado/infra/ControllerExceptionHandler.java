57838e17f3f947336d01592901e781740501c740
Adna Backman <adnabackman@gmail.com>
2025-08-25 09:54:12 -0300

    create: Controller Exception Handler

--- src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
+++ src/main/java/com/picpaysimplificado/infra/ControllerExceptionHandler.java
@@ -1,5 +1,27 @@
 package com.picpaysimplificado.infra;
 
-public class ControllerExceptionHandler {
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.picpaysimplificado.dtos.ExceptionDTO;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception) {
+		ExceptionDTO exceptionDTO = new ExceptionDTO ("Usuario já cadastrado", "400");
+		return ResponseEntity.badRequest().body(exceptionDTO);
+	}
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity threat404(EntityNotFoundException exception) {
+		return ResponseEntity.notFound().build();
+	}
+	
 }
+	
